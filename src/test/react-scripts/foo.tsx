84fa7fb6eebbdb4132a0ce0a5ed163d5192eb887
Miguel Pineda <a.pinedavegamiguel@gmail.com>
2020-11-08 16:07:28 -0500

    upgrade typescript 3.8.3

--- src/test/react-scripts/foo.tsx
+++ src/test/react-scripts/foo.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import type { Foo } from "./foo";
 
 function HelloWorld() {
+  const foo: Foo = {
+    bar: "",
+  };
+
   return <div>Hello foo!!!</div>;
 }
 
